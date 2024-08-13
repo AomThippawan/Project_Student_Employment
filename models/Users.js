72335fc53fb736586574b53c21ed0523546d6a1e
AomThippawan <157473472+AomThippawan@users.noreply.github.com>
2024-08-13 20:20:13 +0700

    Update Users.js

--- models/Users.js
+++ models/Users.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
-//begin change _id to NUmber 1 2 3 ....
+//begin change _id to Number 1 2 3 ....
 const CounterSchema = new mongoose.Schema({
-    _id: String,
+    User_id: String,
     sequence_value: {type: Number, default: 0}                    
 });
 const Counter = mongoose.model("Counter", CounterSchema);
@@ -18,7 +18,12 @@ async function NextSequnceValue(SequenceName) {
 
 //models Users
 const UserSchema = new mongoose.Schema({
-   
+    User_id     : {type:Number},
+    First_Name  : {type:String,require:true},
+    Last_Name   : {type:String,require:true},
+    Email       : {type:String,require:true},
+    Phone       : {type:String,require:true},
+    Password    : {type:String,require:true},
 
     
 })
